619dc03c7fa2b8e077f58b6ab1658142ce850a2e
SaurabhJape5 <saurabhjapesj.com>
2025-06-30 20:06:17 +0530

    Fix: added base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/', // ✅ This is important for Netlify
   plugins: [react()],
   build: {
     outDir: 'dist',
